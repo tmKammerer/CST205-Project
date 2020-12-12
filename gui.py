30f782f82cca91dba959e948dffb5865e7a285f3
Yessica Torrez Hernandez <yessi@Yessicas-MacBook-Pro.local>
2020-12-12 14:04:23 -0800

    corrections

--- gui.py
+++ gui.py
@@ -7,29 +7,30 @@ class MyWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-
+        #Window Title
         self.setWindowTitle("Color Calculator")
-        self.label3 = QLabel('Hello! Enter your RGB value or HEX values to find your desired color.  ')
-        self.label1 = QLabel('Input R: ')
-        self.line_edit = QLineEdit()
-        self.labelg = QLabel('Input G: ')
+
+        self.label_title = QLabel('Hello! Enter your RGB value or HEX values to find your desired color.  ')
+        self.label_r = QLabel('Input R: ')
+        self.liner_edit = QLineEdit()
+        self.label_g = QLabel('Input G: ')
         self.lineg_edit = QLineEdit()
-        self.labelb = QLabel('Input B: ')
+        self.label_b = QLabel('Input B: ')
         self.lineb_edit = QLineEdit()
 
-        self.label2 = QLabel('Input: ')
-        self.line2_edit = QLineEdit()
+        self.label_hex = QLabel('Input: ')
+        self.lineh_edit = QLineEdit()
 
         vbox1 = QVBoxLayout()
-        vbox1.addWidget(self.label3)
-        vbox1.addWidget(self.label1)
-        vbox1.addWidget(self.line_edit)
-        vbox1.addWidget(self.labelg)
+        vbox1.addWidget(self.label_title)
+        vbox1.addWidget(self.label_r)
+        vbox1.addWidget(self.liner_edit)
+        vbox1.addWidget(self.label_g)
         vbox1.addWidget(self.lineg_edit)
-        vbox1.addWidget(self.labelb)
+        vbox1.addWidget(self.label_b)
         vbox1.addWidget(self.lineb_edit)
-        vbox1.addWidget(self.label2)
-        vbox1.addWidget(self.line2_edit)
+        vbox1.addWidget(self.label_hex)
+        vbox1.addWidget(self.lineh_edit)
 
         gbox1 = QGroupBox('Welcome to the color calculator!')
         gbox1.setLayout(vbox1)
