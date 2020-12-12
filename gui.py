7efe4cf1d1eee96e1e0d92b8e0811c95487f3e40
Yessica Torrez Hernandez <yessi@Yessicas-MacBook-Pro.local>
2020-12-12 15:20:46 -0800

    radio button

--- gui.py
+++ gui.py
@@ -1,27 +1,50 @@
 import sys
-from PySide2.QtWidgets import (QApplication, QWidget, QLabel, QDialog, QGroupBox, 
-                                  QHBoxLayout, QVBoxLayout, QPushButton, QLineEdit)
-from PySide2.QtCore import Slot                            
+from PySide2.QtWidgets import (QApplication, QWidget, QLabel, QDialog, QGroupBox,
+                               QHBoxLayout, QVBoxLayout, QPushButton, QLineEdit,
+                               QRadioButton)
+from PySide2.QtCore import Slot
+
+# New imports for adding radio button
+
 
 class MyWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-        #Window Title
+        # Window Title
         self.setWindowTitle("Color Calculator")
 
-        self.label_title = QLabel('Hello! Enter your RGB value or HEX values to find your desired color.  ')
+        # Need input of R,G,B in order to get number
+        self.label_title = QLabel(
+            'Hello! Enter your RGB value or HEX values to find your desired color.  ')
+
+        # Created a box layout
+        vbox1 = QVBoxLayout()
+
+
+        # adding radio button
+        radiobutton = QRadioButton("RGB")
+        radiobutton.setChecked(True)
+        radiobutton.type = "RGB"
+        radiobutton.toggled.connect(self.onColorTypeClicked)
+        vbox1.addWidget(radiobutton)
+
+        radiobutton = QRadioButton("HEX")
+        radiobutton.type = "HEX"
+        radiobutton.toggled.connect(self.onColorTypeClicked)
+        vbox1.addWidget(radiobutton)
+
         self.label_r = QLabel('Input R: ')
         self.liner_edit = QLineEdit()
         self.label_g = QLabel('Input G: ')
         self.lineg_edit = QLineEdit()
         self.label_b = QLabel('Input B: ')
         self.lineb_edit = QLineEdit()
-
-        self.label_hex = QLabel('Input: ')
+        
+        # This line_edit input is for Hex
+        self.label_hex = QLabel('Input Hex: ')
         self.lineh_edit = QLineEdit()
 
-        vbox1 = QVBoxLayout()
         vbox1.addWidget(self.label_title)
         vbox1.addWidget(self.label_r)
         vbox1.addWidget(self.liner_edit)
@@ -34,11 +57,10 @@ class MyWindow(QWidget):
 
         gbox1 = QGroupBox('Welcome to the color calculator!')
         gbox1.setLayout(vbox1)
-        
+
         mbox = QVBoxLayout()
         mbox.addWidget(gbox1)
 
-        
         self.my_btn = QPushButton("Submit")
         self.my_lbl = QLabel('Button not clicked')
         self.my_btn.clicked.connect(self.on_click)
@@ -54,6 +76,12 @@ class MyWindow(QWidget):
     def on_click(self):
         self.my_lbl.setText('Button clicked')
         self.repaint()
+
+    def onColorTypeClicked(self):
+        radioButton = self.sender()
+        if radioButton.isChecked():
+            print("Color Type is %s" % (radioButton.type))
+
 
 app = QApplication([])
 my_win = MyWindow()
